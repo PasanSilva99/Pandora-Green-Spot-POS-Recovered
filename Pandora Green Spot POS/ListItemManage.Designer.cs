94612ee767bc9dfeee8c654448cfd269c7fbfe75
Pasan Anuththara <pasananuththara19@gmail.com>
2020-08-07 19:43:50 +0530

    search updated

---
+++ Pandora Green Spot POS/ListItemManage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Pandora_Green_Spot_POS
+{
+    partial class ListItemManage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ListItemManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ListItemManage";
+            this.Size = new System.Drawing.Size(630, 98);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
