c72fde4d64a2f9237431ce93688f3406d6abdd69
Pasan Anuththara <pasananuththara19@gmail.com>
2020-08-10 23:20:32 +0530

    tyyr

--- Pandora Green Spot POS/Reports.Designer.cs
+++ Pandora Green Spot POS/Reports.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 547);
+            this.Name = "Reports";
             this.Text = "Reports";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
